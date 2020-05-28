25807287b27539b7e34deb27af4dc67fde2c672e
Cody Burker <cody.burker@franklincollege.edu>
2020-05-27 22:12:41 -0400

    Implemented testing of persistance function

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 
 func main() {
 	for {
+		persistenceTest()
+
 		// fmt.Print(">>")
 		// var inputInt int64
 		// fmt.Scan(&inputInt)
@@ -15,8 +17,25 @@ func main() {
 		// fmt.Print("Persistance:\t")
 		// fmt.Println(persistenceResult)
 		// fmt.Println("")
-		// Git test
 
+	}
+}
+
+// TODO: Write a test to make sure that my changes don't mess with it
+func persistenceTest() {
+	// Use known results from here: https://mathworld.wolfram.com/MultiplicativePersistence.html
+	testOutputs := [11]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+	testInputs := [11]int{10, 25, 39, 77, 679, 6788, 68889, 2677889, 26888999, 3778888999, 277777788888899}
+	for index, inputDigit := range testInputs {
+		if persistence(int64(inputDigit)) != int8(testOutputs[index]) {
+			fmt.Println("Error:")
+			fmt.Print("Input:\t")
+			fmt.Println(inputDigit)
+			fmt.Print("Got:\t")
+			fmt.Println(persistence(int64(inputDigit)))
+			fmt.Print("Expected:\t")
+			fmt.Println(testOutputs[index])
+		}
 	}
 }
 
@@ -29,8 +48,8 @@ func persistence(n int64) int8 {
 		// Get next step:
 		p = p + 1
 		n = doMultiply(n)
-		fmt.Print("\tStep:\t")
-		fmt.Println(n)
+		// fmt.Print("\tStep:\t")
+		// fmt.Println(n)
 		if n < 10 {
 			break
 		}
@@ -47,7 +66,8 @@ func doMultiply(n int64) int64 {
 	baseValue = 10
 
 	for {
-		digitsSlice = append(digitsSlice, n%baseValue)
+		newDigit := n % baseValue
+		digitsSlice = append(digitsSlice, newDigit)
 		n = n / baseValue
 		if n == 0 {
 			break
